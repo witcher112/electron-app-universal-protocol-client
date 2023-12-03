c7335fb40d6b1232b6b4374cf231766ee9ed3299
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2023-12-03 23:34:30 +0100

    Update example

--- example/main.js
+++ example/main.js
@@ -2,14 +2,23 @@ const electron = require('electron');
 
 const electronAppUniversalProtocolClient = require('electron-app-universal-protocol-client').default;
 
+// Usually you want to run only one instance of your app to handle all requests in one context
+
+if (!electron.app.requestSingleInstanceLock()) {
+  electron.app.exit(0);
+}
+
 electron.app.on('ready', async () => {
+  // Prepare your application, create windows etc.
+
   const window = new electron.BrowserWindow();
   window.loadURL('https://google.com');
+
+  // When you're ready to handle protocol requests, register the handler and initialize the client
 
   electronAppUniversalProtocolClient.on(
     'request',
     async (requestUrl) => {
-
       // Handle the request
 
       await electron.dialog.showMessageBox(
@@ -23,8 +32,8 @@ electron.app.on('ready', async () => {
 
   await electronAppUniversalProtocolClient.initialize({
     protocol: 'your-app-id',
-    mode: 'development', // If running in dev mode, otherwise use 'production' or skip
+    mode: 'development', // Make sure to use 'production' when script is executed in bundled app
   });
 
-  // From now accessing URL like your-app-id://test?foo=bar should open your app and trigger the 'request' event
+  // At this point accessing URLs like your-app-id://test?foo=bar should open your app and trigger the 'request' event
 });
