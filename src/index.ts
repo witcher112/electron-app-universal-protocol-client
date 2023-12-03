ee063e18581cdc3f2a01865c605c06d1396cf077
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2023-12-03 23:27:26 +0100

    Add support for dev mode on Linux + Xfce

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as electron from 'electron';
 import * as os from 'os';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as crypto from 'crypto';
+import { execSync } from 'child_process';
 import { EventEmitter } from 'events';
 import TypedEmitter from 'typed-emitter';
 
@@ -170,11 +172,56 @@ class ElectronAppUniversalProtocolClient extends (EventEmitter as new () => Type
 
       if (mode === `development`) {
 
+        const electronAppMainScriptPath = path.resolve(process.argv[1]);
+
+        if (os.platform() === `linux`) {
+
+          // HACK: As `electron.app.setAsDefaultProtocolClient` is based on `xdg-settings set default-url-scheme-handler` which is not supported on Xfce, we manually create new .desktop entry and use `xdg-mime` to make it default handler for protocol URLs.
+
+          try {
+
+            const electronAppDesktopFileName = `electron-app-universal-protocol-client-${crypto.createHash(`md5`).update(`${process.execPath}${electronAppMainScriptPath}`).digest(`hex`)}.desktop`;
+
+            const electronAppDesktopFilePath = path.resolve(
+              electron.app.getPath(`home`),
+              `.local`,
+              `share`,
+              `applications`,
+              electronAppDesktopFileName,
+            );
+
+            fs.mkdirSync(
+              path.dirname(electronAppDesktopFilePath),
+              {
+                recursive: true,
+              },
+            );
+
+            fs.writeFileSync(
+              electronAppDesktopFilePath,
+              [
+                `[Desktop Entry]`,
+                `Name=Electron (pid: ${process.pid})`,
+                `Exec=${process.execPath} ${electronAppMainScriptPath} %u`,
+                `Type=Application`,
+                `Terminal=false`,
+                `MimeType=x-scheme-handler/${protocol};`
+              ].join(`\n`),
+            );
+
+            execSync(`xdg-mime default ${electronAppDesktopFileName} x-scheme-handler/${protocol}`);
+
+          } catch {
+
+            // ignore
+          }
+        }
+
         electron.app.setAsDefaultProtocolClient(
           protocol,
           process.execPath,
           [
-            path.resolve(process.argv[1]),
+            electronAppMainScriptPath,
           ],
         );
 
